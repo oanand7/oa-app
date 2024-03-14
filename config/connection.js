44a9db60ed2791b06b87f959e5f5e86c72863f97
Osho Anand <145639401+oanand7@users.noreply.github.com>
2024-03-14 06:36:08 +0530

    database_name_URL

--- config/connection.js
+++ config/connection.js
@@ -32,8 +32,8 @@ async function getConnectionInfo() {
   const DATABASE_NAME = process.env.DATABASE_NAME || "azure-todo-app";
 
   return {
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_NAME: process.env.DATABASE_NAME
+    DATABASE_URL: "mongodb://oa-app-server:sYKrACYoG26Ii5NZRsR0CPh7DiGNYyu5VmmaWvwKgqunfba7fZpjXKub0nVCliHGIwRpCY5SW3KbACDbaL5PaA==@oa-app-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@oa-app-server@",
+    DATABASE_NAME: "oa-app-database"
   }
 }
 
